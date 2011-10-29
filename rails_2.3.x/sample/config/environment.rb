b4a090753f4a3f43369129aa4f822ace7681b04e
Michal Czyz <michalczyz@gmail.com>
2011-10-29 18:47:04 +0200

    it's better to use plugin then gem

--- rails_2.3.x/sample/config/environment.rb
+++ rails_2.3.x/sample/config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'diff-lcs', :lib => false
   config.gem 'cucumber_fm-core', :lib => 'cucumber_feature_manager', :version => '0.1'
-  config.gem 'cucumber_fm-plugin', :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
