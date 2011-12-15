b018c1fe7cd2dff900a7ba07c4549e5009a67a1f
Michal Czyz <michalczyz@gmail.com>
2011-12-16 00:08:58 +0100

    adding base rails 3.1.3 app

---
+++ rails_3.1.x/app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '3a9e1f17b8cbfdc15aa58feb2548a173f115300ebd32fbe534601937dfc2f9c7640117e0315466da0025cc4a38b8a09fb277e0dd4f93faceee7389b70e769662'
